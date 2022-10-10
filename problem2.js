9c6ab211250ddac984448784b16a407482b610bc
WBwithSuraj <surajkrsr98@gmail.com>
2022-10-10 19:40:22 +0530

    Initial Commit

---
+++ problem2.js
@@ -0,0 +1,9 @@
+//this is my name
+let name = "Suraj Kumar Kamat";
+console.log(name)
+//This is my father's name
+ name = "Mangnand Kamat";
+console.log(name)
+//This is my mother's name
+ name = "Nilam Devi";
+console.log(name)
---
+++ problem4.js
@@ -0,0 +1,21 @@
+let name = "Suraj Kumar"
+console.log(name)
+let school = "Delhi Public School"
+console.log(school)
+let grade = "A";
+console.log(grade)
+let section = "A";
+console.log(section)
+let rollno = 24;
+console.log(rollno)
+let marks ={
+   science : "56",
+   math : "78",
+  english : "99"
+}
+console.log(marks)
+
+   
+  
+ 
+  
